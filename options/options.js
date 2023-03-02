bd22748a8876a08e38bc8a46b19f20895d0897de
Crowdbotics <team@crowdbotics.com>
2023-03-02 22:45:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "recipeful-39202",
+  url: "https://recipeful-39202.botics.co",
+  api: "https://recipeful-39202.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
